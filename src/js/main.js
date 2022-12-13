0be807823a60fccb54ba09fe3155bf2a1a28a414
himahinulabid@gmail.com <himahinulabid@gmailcom>
2022-12-13 18:33:48 +0600

    mobile navigation created

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
-console.log("script works");
-let nav= document.getElementById("navigation");
+const nav= document.getElementById("navigation");
+const hamBarOpen= document.querySelector(".ham-bar-container");
+const hambarClosebtn=document.getElementById("nav-close");
+const mobileNavSection= document.querySelector(".mobile-nav");
+
 document.addEventListener("scroll" , (event)=>{
     let pageScrolled= window.pageYOffset;
     if(pageScrolled > 100){
@@ -12,3 +15,16 @@ document.addEventListener("scroll" , (event)=>{
         nav.style.animation="";
     }
 });
+
+
+
+// NAVIGATIN OPEN AND CLOSE BUTTON
+hamBarOpen.addEventListener("click", ()=>{
+    mobileNavSection.classList.remove("display-none");
+    document.querySelector("html, body").style.overflowY="hidden";
+});
+
+hambarClosebtn.addEventListener("click", ()=>{
+    mobileNavSection.classList.add("display-none");
+    document.querySelector("html, body").style.overflowY="scroll";
+})
